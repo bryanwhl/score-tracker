2d681eb867ca2eb6176694b34ab86c73f59ec811
bryanwhl <bryanwhl1999@gmail.com>
2022-08-15 23:14:20 -0700

    feat: add appbar and format homepage container

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,35 @@ import {
   Route, 
 } from "react-router-dom";
 import HomePage from "./screens/HomePage";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {
+  AppBar,
+	CssBaseline,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <SportsSoccerIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Score Tracker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- client/src/screens/HomePage.js
+++ client/src/screens/HomePage.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import {
-	CssBaseline,
-	Typography,
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
 } from '@mui/material';
-import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 
 const HomePage = () => {
   return (
     <div style={{ padding: 16, margin: 'auto', maxWidth: 700 }}>
-      <CssBaseline />
-
-      <SportsSoccerIcon />
-      <Typography variant="h4" align="center" component="h1" gutterBottom>
-          Score Tracker
-      </Typography>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Stack
+            sx={{ pt: 4 }}
+            direction="column"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Stack
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <TextField id="outlined-basic" label="Team Information" variant="outlined" sx={{ width: "80%" }}/>
+              <Button variant="contained" component="label">
+                Submit
+              </Button>
+            </Stack>
+          </Stack>
+        </Container>
+      </Box>
     </div>
   )
 }
